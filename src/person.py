f8954e9659361c8277106f413e92a3bbe5a18693
muv1ton <2004.vanek.2004@mail.ru>
2024-11-23 06:31:39 +0300

    Добавлен шаблон Singletone

--- src/person.py
+++ src/person.py
@@ -1,6 +1,15 @@
 class Person:
+    # Singletone
+    __instance = None
+    def __new__(cls, *args, **kwgs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+    
+    # Конструктор
     def __init__(self, name):
         self.name = name
     
+    # Метод выводящий имя объекта класса Person
     def Print(self):
         print(self.name)
